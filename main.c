aca03b8fb82000d4fd77389f69ecd41df46a09a5
Soufyane <elomranisoufyan@gmail.com>
2025-09-02 15:00:47 +0200

    ex sphere v1

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int main(void)
-{
-printf("hello, world\n");
-return 0;
+#include <math.h>
+int main(void) {
+    int Rayon = 10;
+    const double PI = 3.1415926535897;
+    float volume = 0 ;
+
+    volume = (4.0 / 3.0) * PI * pow(Rayon,3);
+
+    printf("The volume of the sphere is : %f",volume);
+
+
+
+
+return 0; 
 }
