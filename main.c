ddaba5ad03e168a333c61547b9402a3fcc68aa53
Soufyane <elomranisoufyan@gmail.com>
2025-09-02 15:04:45 +0200

    sphere volume v2

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 int main(void) {
-    int Rayon = 10;
+    int Rayon = 0;
     const double PI = 3.1415926535897;
     float volume = 0 ;
 
-    volume = (4.0 / 3.0) * PI * pow(Rayon,3);
+
+    printf("Enter a value for R : ");
+    scanf("%d",&Rayon);
+
+    volume = (4.0f / 3.0f) * PI * pow(Rayon,3);
 
     printf("The volume of the sphere is : %f",volume);
 
